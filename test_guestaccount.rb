03dc19c68952baf6f742513a3bdc97c292276223
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2019-07-07 16:42:35 +0100

    refactored create_triples

--- test_guestaccount.rb
+++ test_guestaccount.rb
@@ -8,6 +8,7 @@ class TestGuestAccount < Test::Unit::TestCase
   def test_guestaccount
     parse = GuestAccount.new
     @base_accounts = []
+    # use one for emails other for pairs
    # @base_accounts =  parse.parse_accounts(ENV['email_messages'])
     @base_accounts =  parse.parse_pairs(ENV['email_messages'])
     parse.generate_cvs
